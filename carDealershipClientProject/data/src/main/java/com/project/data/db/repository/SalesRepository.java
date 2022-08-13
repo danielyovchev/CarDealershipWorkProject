c08187309c943a64d2e7e79c482b1e0781e990f5
danielyovchev <daniel.yovchev@gmail.com>
2022-08-13 18:42:50 +0300

    pom fix

---
+++ carDealershipClientProject/data/src/main/java/com/project/data/db/repository/SalesRepository.java
@@ -0,0 +1,9 @@
+package com.project.data.db.repository;
+
+import com.project.data.db.entity.Sales;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SalesRepository extends JpaRepository<Sales, Long> {
+}
