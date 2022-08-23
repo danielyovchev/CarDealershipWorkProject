f2583814177fee00c6fcc37a54fc0225502aa947
danielyovchev <daniel.yovchev@gmail.com>
2022-08-23 18:14:49 +0300

    sell operation testing

--- carDealershipClientProject/api/src/main/java/com/project/api/model/carSellModel/CarSellRequest.java
+++ carDealershipClientProject/api/src/main/java/com/project/api/model/carSellModel/CarSellRequest.java
@@ -2,11 +2,13 @@ package com.project.api.model.carSellModel;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.api.base.OperationInput;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
-@Getter
+@Getter @Builder @EqualsAndHashCode
 public class CarSellRequest implements OperationInput {
     private String vin;
     private String dealType;
---
+++ carDealershipClientProject/core/src/test/java/com/project/core/processor/CarSellOperationCoreTest.java
@@ -0,0 +1,75 @@
+package com.project.core.processor;
+
+import com.price.api.model.PriceRequest;
+import com.price.api.model.PriceResponse;
+import com.project.api.model.carSellModel.CarSellRequest;
+import com.project.data.crud.interfaces.CreateSaleService;
+import com.project.data.db.entity.Customer;
+import com.project.data.db.repository.CustomerRepository;
+import com.project.data.domain.interfaces.GetPriceService;
+import com.project.data.domain.interfaces.MapCarFromApiService;
+import com.project.data.domain.model.CarDomainModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+
+class CarSellOperationCoreTest {
+    @Mock
+    private MapCarFromApiService carFromApiService;
+    @Mock
+    private CreateSaleService createSaleService;
+    @Mock
+    private GetPriceService getPriceService;
+    @Mock
+    private CustomerRepository customerRepository;
+    @InjectMocks
+    private CarSellOperationCore carSellOperationCore;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void process() {
+        final CarSellRequest carSellRequest = CarSellRequest.builder()
+                .vin("15646565461dasd")
+                .employeeId(1)
+                .customerId(1)
+                .dealType("cash")
+                .months(40)
+                .date(LocalDate.of(2021,6,20)).build();
+        final CarDomainModel car = CarDomainModel.builder()
+                .make("Ford")
+                .model("Focus")
+                .price(6000.0)
+                .build();
+        final Customer customer = new Customer();
+        customer.setFirstName("Ivan");
+        customer.setLastName("Ivanov");
+        customer.setBought(5);
+        customer.setId(1L);
+        final PriceRequest priceRequest = PriceRequest.builder()
+                .basePrice(car.getPrice())
+                .customerPurchasedCars(5)
+                .months(carSellRequest.getMonths())
+                .type(carSellRequest.getDealType())
+                .build();
+        final PriceResponse priceResponse = PriceResponse.builder()
+                .price(7050.20)
+                .message("etc")
+                .build();
+        Mockito.when(carFromApiService.getCar(carSellRequest.getVin())).thenReturn(car);
+        Mockito.when(customerRepository.findById(customer.getId())).thenReturn(Optional.of(customer));
+        Mockito.when(getPriceService.getPriceFromService(priceRequest)).thenReturn(priceResponse);
+        Mockito.when(createSaleService.createSale(carSellRequest, 6000.00)).thenReturn(null);
+
+        assertEquals(7050.20, carSellOperationCore.process(carSellRequest).get().getPrice());
+    }
+}
