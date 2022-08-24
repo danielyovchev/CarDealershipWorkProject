a13ce95e09e70bfea9bec0b510d1734e6f543ab0
danielyovchev <daniel.yovchev@gmail.com>
2022-08-24 14:44:13 +0300

    core testing

---
+++ carDealershipClientProject/core/src/test/java/com/project/core/processor/GetAllCarsOperationCoreTest.java
@@ -0,0 +1,58 @@
+package com.project.core.processor;
+
+import com.project.api.model.carsByParam.AllCarRequest;
+import com.project.api.model.carsByParam.CarListResponse;
+import com.project.data.crud.interfaces.GetAllCars;
+import com.project.data.domain.mapper.DomainToResponseMapper;
+import com.project.data.domain.model.CarDomainModel;
+import com.project.data.domain.model.CarListDomainResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetAllCarsOperationCoreTest {
+    @Mock
+    private GetAllCars getAllCars;
+    @Mock
+    private DomainToResponseMapper mapper;
+    @InjectMocks
+    private GetAllCarsOperationCore getAllCarsOperationCore;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void process() {
+        CarDomainModel car1 = new CarDomainModel();
+        car1.setMake("Ford");
+        car1.setFuel("petrol");
+        car1.setDisplacement(1560);
+        car1.setEconomy(8.5);
+        car1.setGears(5);
+        car1.setMileage(165406);
+        car1.setStatus("available");
+        car1.setDrivenWheels("front");
+        car1.setTorque(150);
+        car1.setPrice(3500.0);
+        car1.setTransmission("manual");
+        car1.setHorsepower(100);
+        car1.setYear(1995);
+        car1.setModel("Escort");
+        car1.setVin("1FASP11J6TW112004");
+        CarListDomainResponse carListDomainResponse =   CarListDomainResponse
+                .builder()
+                .carDomainModelList(List.of(car1))
+                .build();
+        Mockito.when(getAllCars.getAllCars()).thenReturn(carListDomainResponse);
+        AllCarRequest allCarRequest = new AllCarRequest();
+        assertEquals(getAllCarsOperationCore.process(allCarRequest).get().getClass(), CarListResponse.class);
+    }
+}
